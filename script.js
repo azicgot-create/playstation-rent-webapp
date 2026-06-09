6a44e01f3cbd992da4ec5320355565b6ecf6a5aa
azicgot-create <azicgot@gmail.com>
2026-06-10 02:38:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const tg = window.Telegram.WebApp;
-
 tg.expand();
 
-console.log("Mini App запущен");
+function show(page){
+
+    document.querySelectorAll(".page").forEach(p => {
+        p.classList.remove("active");
+    });
+
+    document.getElementById(page).classList.add("active");
+
+    document.querySelectorAll(".nav button").forEach(b => {
+        b.classList.remove("active");
+    });
+
+    document.querySelector(`[data-page="${page}"]`).classList.add("active");
+}
+
+document.querySelectorAll(".nav button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        show(btn.dataset.page);
+    });
+});
+
+// Telegram user
+if(tg.initDataUnsafe.user){
+
+    document.getElementById("name").innerText =
+        tg.initDataUnsafe.user.first_name;
+
+    document.getElementById("id").innerText =
+        "ID: " + tg.initDataUnsafe.user.id;
+}
+
+show("home");
