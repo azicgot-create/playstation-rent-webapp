8c4203b83b5e8465b320f47603471025e214e416
azicgot-create <azicgot@gmail.com>
2026-06-10 13:08:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ const tg = window.Telegram.WebApp;
 
 tg.expand();
 
+const user = tg.initDataUnsafe.user;
+
+if (user) {
+
+    const profilePage = document.getElementById("profile-page");
+
+    profilePage.innerHTML = `
+
+        <h1>Профиль</h1>
+
+        <div class="profile-card">
+
+            <img
+                src="${user.photo_url || 'https://placehold.co/150'}"
+                class="avatar"
+            >
+
+            <h2>${user.first_name || ''} ${user.last_name || ''}</h2>
+
+            <p>@${user.username || 'нет username'}</p>
+
+            <p>ID: ${user.id}</p>
+
+        </div>
+
+        <div class="card">
+            История бронирований появится позже
+        </div>
+
+    `;
+}
+
 function showPage(pageId, button) {
 
     document.querySelectorAll(".page").forEach(page => {
